a47cebd5a46a2b48cd251729e9251e6f879f1c32
Mr-Gump <2424659013@qq.com>
2020-06-10 18:46:18 +0800

    6.10

---
+++ Get_Libarary_Seat.py
@@ -0,0 +1,93 @@
+import requests,time
+from bs4 import BeautifulSoup
+
+Cookie = input('请输入Cookie:')
+
+headers = {
+    'Cookie':Cookie
+}
+
+url = "http://rg.lib.xjtu.edu.cn:8010/qseat?sp=north2east"
+
+def update_seat(room,seat):
+    update_url = "http://rg.lib.xjtu.edu.cn:8010/updateseat/?kid=" +seat+ "&sp=" + room
+    res = requests.get(update_url,headers=headers)
+    soup = BeautifulSoup(res.text,'html.parser')
+    return(soup.find(class_='alert').text)
+
+def choose_seat(room,seat):
+    choose_url = "http://rg.lib.xjtu.edu.cn:8010/seat/?kid=" +seat+ "&sp=" + room
+    res = requests.get(choose_url,headers=headers)
+    soup = BeautifulSoup(res.text,'html.parser')
+    return(soup.find(class_='alert').text)
+
+def get_seat(room,available_seats):
+    room_url = "http://rg.lib.xjtu.edu.cn:8010/qseat?sp=" + room
+    message = requests.get(room_url,headers=headers).json()
+    seats = message['seat']
+    seats.pop('')
+    for seat in seats:
+        if seats[seat] == 0:
+            available_seats.append((room,seat))
+
+def get_my_status():
+    my_url = "http://rg.lib.xjtu.edu.cn:8010/my/"
+    html = requests.get(my_url,headers=headers).text
+    soup = BeautifulSoup(html,'html.parser')
+    items = soup.find_all(class_="bs-calltoaction")
+    for item in items[:1]:
+        print('-----------------------------------')
+        info = item.find(class_='cta-contents')
+        status = item.find(class_="cta-button")
+        print(info.text.strip())
+        print(status.text.strip()[:-5])
+        print('-----------------------------------')
+
+def get_available_seats():
+    available_seats = []
+    message = requests.get(url,headers=headers).json()
+
+    rooms = message['scount']
+    rooms.pop('')
+
+    for room in rooms:
+        name = room
+        status = rooms[room][1]
+
+        if status > 0 :
+            get_seat(name,available_seats)
+    return available_seats
+
+flag = 0
+while(True):
+    if flag == 0 :
+        print('正在获取空余座位列表...')
+        available_seats_list = get_available_seats()
+        count = 1
+        for available_seat in available_seats_list:
+            print(str(count),'.位置:',available_seat[0],end='    ')
+            print('座位号:',available_seat[1])
+            count += 1
+
+        if len(available_seats_list) >= 0:
+            index = eval(input('请输入选择座位的编号:'))
+            msg = choose_seat(available_seats_list[index-1][0],available_seats_list[index-1][1])
+            print()
+            print(msg[3:])
+            if msg[3:].strip() == '抱歉，该座位已被预约':
+                print('-----------------------')
+                continue
+            else :
+                flag = 1
+                print('正在锁定座位...请勿关闭')
+        else :
+            print('当前无空位!')
+        get_my_status()
+    
+    msg = choose_seat(available_seats_list[index-1][0],available_seats_list[index-1][1])
+    time.sleep(1)
+    print()
+    print(msg[3:])
+    
+
+
---
+++ README.md
@@ -0,0 +1,18 @@
+# 图书馆抢座程序
+## 功能
++ 1.查询实时空座位
++ 2.根据用户选择抢座
++ 3.长时间锁定座位,直到用户入馆(开馆前预约则必定成功占位)
++ 4.查询用户当前状态
+
+## 使用步骤
++ 1. 打开Chrome浏览器,访问 http://rg.lib.xjtu.edu.cn:8010/ruguan 并登录
+[![tTfc7j.md.png](https://s1.ax1x.com/2020/06/10/tTfc7j.md.png)](https://imgchr.com/i/tTfc7j)
++ 2. 点击右键,检查,并刷新页面
+[![tTfh90.md.png](https://s1.ax1x.com/2020/06/10/tTfh90.md.png)](https://imgchr.com/i/tTfh90)
++ 3. 点击Network
+[![tTfWhq.md.png](https://s1.ax1x.com/2020/06/10/tTfWhq.md.png)](https://imgchr.com/i/tTfWhq)
++ 4. 随便选择一个数据包,复制 Cookie: 后面一串
+[![tTf2As.md.png](https://s1.ax1x.com/2020/06/10/tTf2As.md.png)](https://imgchr.com/i/tTf2As)
++ 5. 运行程序 输入Cookie 开始占座
+[![tThNKU.md.png](https://s1.ax1x.com/2020/06/10/tThNKU.md.png)](https://imgchr.com/i/tThNKU)
